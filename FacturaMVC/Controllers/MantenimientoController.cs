269efa7c35e095433566f31700f8c82a8eff8284
jorge <jsandoval@pucp.edu.pe>
2019-09-28 18:11:38 -0500

    Cambios 28/09/2019

---
+++ FacturaMVC/Controllers/MantenimientoController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FacturaMVC.Controllers
+{
+    public class MantenimientoController : Controller
+    {
+        public ActionResult Concepto()
+        {
+            return View();
+        }
+    }
+}
